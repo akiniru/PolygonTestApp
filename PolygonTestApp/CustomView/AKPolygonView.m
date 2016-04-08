0c2d7e268754dff0162ba14cff73981e086dcfd8
akiniru <akiniru@hotmail.com>
2016-04-08 11:45:01 +0900

    Moving All Points
    Moving All Points

--- PolygonTestApp/CustomView/AKPolygonView.m
+++ PolygonTestApp/CustomView/AKPolygonView.m
@@ -13,10 +13,12 @@
 
 @interface AKPolygonView ()
 
-@property (nonatomic, strong) NSMutableArray *pathArray;
 @property (nonatomic, strong) UIColor *fillColor;
+@property (nonatomic, strong) NSMutableArray *pathArray;
+@property (nonatomic, strong) UILongPressGestureRecognizer *gestureRecognizer;
 @property (nonatomic, assign) NSInteger closestPointIndex;
 @property (nonatomic, assign) CGFloat pointCount;
+@property (nonatomic, assign) CGPoint touchStartPoint;
 
 @end
 
@@ -27,16 +29,35 @@
     NSLog(@"called");
     if ((self = [super initWithFrame:frame]))
     {
-        // Clear background to ensure the content view shows through.
-        self.backgroundColor = [UIColor grayColor];
-        self.pathArray = [NSMutableArray array];
-        self.pointCount = DEFAULT_POINT_COUNT;
-        self.fillColor = [UIColor colorWithRed:.5 green:.5 blue:.5 alpha:.9];
-        
-        [self initAllPoints];
+        [self setup];
     }
     
     return self;
+}
+
+- (void)dealloc
+{
+    NSLog(@"called");
+    self.fillColor = nil;
+    
+    [self.pathArray removeAllObjects];
+    self.pathArray = nil;
+    
+    [self removeGestureRecognizer:self.gestureRecognizer];
+    self.gestureRecognizer = nil;
+}
+
+- (void)setup
+{
+    self.backgroundColor = [UIColor grayColor];
+    self.fillColor = [UIColor colorWithRed:.5 green:.5 blue:.5 alpha:.9];
+    self.pathArray = [NSMutableArray array];
+    self.pointCount = DEFAULT_POINT_COUNT;
+    
+    UILongPressGestureRecognizer *gestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(actionLongPress:)];
+    [self addGestureRecognizer:gestureRecognizer];
+    
+    [self initAllPoints];
 }
 
 // Only override drawRect: if you perform custom drawing.
@@ -89,8 +110,50 @@
 
 - (void)initAllPointsWithPointCount:(NSInteger)count;
 {
+    NSLog(@"called");
     self.pointCount = count;
     [self initAllPoints];
+    
+    [self setNeedsDisplay];
+}
+
+- (void)actionLongPress:(UILongPressGestureRecognizer *)recognizer
+{
+    NSLog(@"called. recognizer.state: %ld", (long)recognizer.state);
+    CGPoint touchPoint = [recognizer locationInView:self];
+    if (recognizer.state == UIGestureRecognizerStateBegan)
+    {
+        self.touchStartPoint = touchPoint;
+        NSString *touchPointString = NSStringFromCGPoint(touchPoint);
+        NSLog(@"touchPointString: %@", touchPointString);
+    }
+    else if (recognizer.state == UIGestureRecognizerStateChanged)
+    {
+        [self moveAllPoints:touchPoint];
+    }
+    else if (recognizer.state == UIGestureRecognizerStateEnded)
+    {
+    }
+}
+
+- (void)moveAllPoints:(CGPoint)touchPoint
+{
+    NSLog(@"called");
+    CGFloat distanceX = touchPoint.x - self.touchStartPoint.x;
+    CGFloat distanceY = touchPoint.y - self.touchStartPoint.y;
+    NSLog(@"distanceX: %f, distanceY: %f", distanceX, distanceY);
+    
+    for (NSInteger i = 0; i < self.pathArray.count; i++) {
+        NSValue *savedValue = [self.pathArray objectAtIndex:i];
+        CGPoint savedPoint = [savedValue CGPointValue];
+        NSLog(@"before savedPoint: %@", NSStringFromCGPoint(savedPoint));
+        savedPoint.x = savedPoint.x + distanceX;
+        savedPoint.y = savedPoint.y + distanceY;
+        NSLog(@"after savedPoint: %@", NSStringFromCGPoint(savedPoint));
+        NSValue *exchangePointValue = [NSValue valueWithCGPoint:savedPoint];
+        [self.pathArray replaceObjectAtIndex:i withObject:exchangePointValue];
+    }
+    self.touchStartPoint = touchPoint;
     
     [self setNeedsDisplay];
 }
